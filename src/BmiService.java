834da81efe44671eef4bb65a279ad08c173df875
Tom <gts-2010@mail.ru>
2023-01-30 22:09:06 +0500

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public double calculate(double m) {
+        double height;
+        height = 1.5;
+        return m / (height * height);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 70;
+        float index = (float) service.calculate(weight);
+        System.out.println("Ваш индекс массы тела составляет: " + index);
+
+    }
+}
